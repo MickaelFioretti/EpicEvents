71f6f629a96d1b8fa37aa5b22990932850c15db6
Mickael Fioretti <fiorettimickael1@gmail.com>
2024-03-08 13:43:30 +0000

    Add model imports to __init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,23 @@
+from .user import User, UserCreate, UserUpdate, UserRead
+from .client import Client, ClientRead, ClientCreate, ClientUpdate
+from .contract import Contract, ContractCreate, ContractUpdate, ContractRead
+from .event import Event, EventCreate, EventUpdate, EventRead
+
+__all__ = [
+    "User",
+    "UserCreate",
+    "UserUpdate",
+    "UserRead",
+    "Client",
+    "ClientRead",
+    "ClientCreate",
+    "ClientUpdate",
+    "Contract",
+    "ContractCreate",
+    "ContractUpdate",
+    "ContractRead",
+    "Event",
+    "EventCreate",
+    "EventUpdate",
+    "EventRead",
+]
