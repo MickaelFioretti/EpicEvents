8dab7f1bab216e653d4c2c678346fbc91f17e6fb
Mickael Fioretti <fiorettimickael1@gmail.com>
2024-03-08 14:58:24 +0000

    Remove stopwatch functionality and related CSS

--- app/main.py
+++ app/main.py
@@ -1,33 +1,17 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer
-from cli.chronometre import Stopwatch
 from cli.login.login import LoginForm
-from textual.containers import ScrollableContainer
 
 
 class MyApp(App):
-    CSS_PATH = "stopwatch.tcss"
     BINDINGS = [
         ("d", "toggle_dark", "Toggle dark mode"),
-        ("a", "add_stopwatch", "Add"),
-        ("r", "remove_stopwatch", "Remove"),
     ]
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
-        yield ScrollableContainer(Stopwatch(), id="timers")
         yield LoginForm()
-
-    def action_add_stopwatch(self) -> None:
-        new_stopwatch = Stopwatch()
-        self.query_one("#timers").mount(new_stopwatch)
-        new_stopwatch.scroll_visible()
-
-    def action_remove_stopwatch(self) -> None:
-        timers = self.query("Stopwatch")
-        if timers:
-            timers.last().remove()
 
     def action_toggle_dark(self) -> None:
         self.dark = not self.dark
