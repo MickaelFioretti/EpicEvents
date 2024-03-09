2f829661b2db3976c91ea9f0e820ae11032575c4
Mickael Fioretti <fiorettimickael1@gmail.com>
2024-03-09 10:33:58 +0000

    Add CRUDUser class and import it in __init__.py

---
+++ app/crud/__init__.py
@@ -0,0 +1,4 @@
+from .crud_user import user
+
+
+__all__ = ["user"]
---
+++ app/crud/crud_user.py
@@ -0,0 +1,44 @@
+from sqlalchemy.orm import Session
+from app.crud.base import CRUDBase
+from app.models.user import User, UserCreate, UserUpdate
+from app.core.security import get_password_hash, verify_password
+from typing import Optional
+from sqlmodel import select
+
+
+class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
+    def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
+        return db.execute(select(User).where(User.email == email)).scalar_one_or_none()
+
+    def create(self, db: Session, *, obj_in: UserCreate) -> User:
+        db_obj = User(
+            email=obj_in.email,
+            hashed_password=get_password_hash(obj_in.password),
+            full_name=obj_in.full_name,
+            is_active=obj_in.is_active,
+            department=obj_in.department,
+        )
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def update(self, db: Session, *, db_obj: User, obj_in: UserUpdate) -> User:
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
+        if "password" in update_data:
+            hashed_password = get_password_hash(update_data["password"])
+            del update_data["password"]
+            update_data["hashed_password"] = hashed_password
+        return super().update(db, db_obj=db_obj, obj_in=update_data)
+
+    def authenticate(self, db: Session, *, email: str, password: str) -> Optional[User]:
+        user = self.get_by_email(db, email=email)
+        if user and verify_password(password, user.hashed_password):
+            return user
+        return None
+
+
+user = CRUDUser(User)
