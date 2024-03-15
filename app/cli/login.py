9b7ce2df99f800263ec2c2b8ff2e6d60b2cedde4
Mickael Fioretti <fiorettimickael1@gmail.com>
2024-03-15 16:10:09 +0000

    Refactor login form in app/cli/login.py

--- app/cli/login.py
+++ app/cli/login.py
@@ -1,12 +1,18 @@
+from textual import on
 from textual.widgets import Static, Button, Input
 from textual.app import ComposeResult
 
 
 class LoginForm(Static):
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        pass
+    @on(Button.Pressed)
+    def on_button_pressed(self) -> None:
+        email = self.query_one("#email", Input).value
+        password = self.query_one("#password", Input).value
+
+        self.query_one("#email", Input).value = ""
+        self.query_one("#password", Input).value = ""
 
     def compose(self) -> ComposeResult:
-        yield Input("Email", id="email")
-        yield Input("Password", id="password")
+        yield Input(placeholder="Email", id="email")
+        yield Input(placeholder="Password", id="password")
         yield Button("Login", id="login", variant="success")
