c821906b4840ad19297cd6be92924a43f4900424
Mickael Fioretti <fiorettimickael1@gmail.com>
2024-03-15 09:40:00 +0000

    Add login form to CLI

---
+++ app/cli/login.py
@@ -0,0 +1,12 @@
+from textual.widgets import Static, Button, Input
+from textual.app import ComposeResult
+
+
+class LoginForm(Static):
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        pass
+
+    def compose(self) -> ComposeResult:
+        yield Input("Email", id="email")
+        yield Input("Password", id="password")
+        yield Button("Login", id="login", variant="success")
