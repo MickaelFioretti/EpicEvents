f5f4687923b6114a0fe6db21f0a655c51fdb3b2f
Mickael Fioretti <fiorettimickael1@gmail.com>
2024-03-15 09:40:28 +0000

    Refactor settings.py to use pydantic_settings

--- app/settings.py
+++ app/settings.py
@@ -1,2 +1,9 @@
-SECRET_KEY = "secret"
-ALGORITHM = "HS256"
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    SECRET_KEY = "secret"
+    ALGORITHM = "HS256"
+
+
+settings = Settings()
