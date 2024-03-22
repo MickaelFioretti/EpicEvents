b94f6f3a35345b65606b2438d75f9c9ea7abd3cb
Mickael Fioretti <fiorettimickael1@gmail.com>
2024-03-22 09:45:56 +0000

    fix: move main.py to top of the app

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer
-from cli.login import LoginForm
+from app.cli.login import LoginForm
 
 
 class MyApp(App):
-    CSS_PATH = "style.tcss"
+    CSS_PATH = "app/style.tcss"
     BINDINGS = [
         ("d", "toggle_dark", "Toggle dark mode"),
     ]
 
     def compose(self) -> ComposeResult:
         yield Header()
-        yield LoginForm(id="login-form")
+        yield LoginForm()
         yield Footer()
 
     def action_toggle_dark(self) -> None:
