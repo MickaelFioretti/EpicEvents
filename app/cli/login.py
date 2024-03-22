8e58ac115ddbfd0f80f3f3214b214ca1f4fe4812
Mickael Fioretti <fiorettimickael1@gmail.com>
2024-03-22 09:46:27 +0000

    feat: Add authentication functionality to login form

--- app/cli/login.py
+++ app/cli/login.py
@@ -1,13 +1,33 @@
 from textual import on
 from textual.widgets import Static, Button, Input
 from textual.app import ComposeResult
+from app.session import get_db
+from app.crud.crud_user import CRUDUser
+from app.models.user import User, UserAuthenticate
 
 
 class LoginForm(Static):
+    def __init__(self) -> None:
+        super().__init__()
+        self.crud_user = CRUDUser(User)
+
     @on(Button.Pressed)
     def on_button_pressed(self) -> None:
         email = self.query_one("#email", Input).value
         password = self.query_one("#password", Input).value
+
+        with get_db() as db:
+            user_info = UserAuthenticate(email=email, password=password)
+
+            user = self.crud_user.authenticate(db, obj_in=user_info)
+
+            if user:
+                # L'authentification a réussi
+                print("Authentification réussie pour :", email)
+                # Ici, vous pouvez naviguer vers une autre page ou effectuer d'autres actions
+            else:
+                # Échec de l'authentification
+                print("Échec de l'authentification")
 
         self.query_one("#email", Input).value = ""
         self.query_one("#password", Input).value = ""
