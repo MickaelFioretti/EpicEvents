82f564133a34a311f1a538ca9416f1bbd4840f15
Mickael Fioretti <fiorettimickael1@gmail.com>
2024-03-22 10:33:00 +0000

    Add session.py and test_db_connection.py files

---
+++ app/session.py
@@ -0,0 +1,16 @@
+from contextlib import contextmanager
+from typing import Generator
+from sqlmodel import create_engine, Session
+
+DATABASE_URL = "postgresql://postgres:password@db/epic_events"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+@contextmanager
+def get_db() -> Generator[Session, None, None]:
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ test_db_connection.py
@@ -0,0 +1,37 @@
+from sqlmodel import create_engine
+from app.session import get_db
+from app.models.user import UserCreate, DepartmentEnum
+from app.crud import user as crud_user
+
+DATABASE_URL = (
+    "postgresql://postgres:password@db/epic_events"  # Assurez-vous que l'URL est correcte
+)
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def test_db():
+    with get_db() as db:
+        # Essayer d'insérer un nouvel utilisateur (comme test)
+        new_user = UserCreate(
+            email="test2@epicevents.com",
+            full_name="test2",
+            hashed_password="changethis",
+            department=DepartmentEnum.guest,
+            is_active=True,
+        )
+
+        crud_user.create(db, obj_in=new_user)
+
+        try:
+            print("Utilisateur inséré avec succès.")
+        except Exception as e:
+            db.rollback()
+            print(f"Erreur lors de l'insertion : {e}")
+
+        # Récupérer tous les utilisateurs et les afficher
+        users = crud_user.get_all(db)
+        print("Liste des utilisateurs :", users)
+
+
+if __name__ == "__main__":
+    test_db()
