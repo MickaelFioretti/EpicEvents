e70405b61b6d622b615deea523b53b70ddd0bb2e
Mickael Fioretti <fiorettimickael1@gmail.com>
2024-05-13 14:51:16 +0000

    feat: Add cursor row selection to DataTable
    The DataTable component in the UserList class now supports cursor row selection. This allows users to select a row in the table and perform actions based on the selected data.

--- app/cli/userList.py
+++ app/cli/userList.py
@@ -22,7 +22,7 @@ class UserList(Static):
             yield Button(
                 "Ajouter un utilisateur", variant="success", name="add_user", classes="box"
             )
-            yield DataTable(classes="box")
+            yield DataTable(classes="box", cursor_type="row")
 
     def on_mount(self) -> None:
         table = self.query_one(DataTable)
@@ -40,10 +40,13 @@ class UserList(Static):
             self.query(DataTable).remove()
             self.query(Button).remove()
             self.query(Grid).remove()
-            self.mount(UserForm())
+            self.mount(UserFormCreate())
+
+    def on_data_table_row_selected(self, event: DataTable.RowSelected) -> None:
+        print(event.cursor_row)
 
 
-class UserForm(Static):
+class UserFormCreate(Static):
     def __init__(self, **kwargs) -> None:
         self.crud_user = CRUDUser(User)
         super().__init__(**kwargs)
@@ -76,6 +79,7 @@ class UserForm(Static):
                     self.query_one("#full_name", Input).value,
                     self.query_one("#email", Input).value,
                     self.query_one("#password", Input).value,
+                    self.query_one("#department", Select).value,
                 ]
             ):
                 self.mount(
@@ -94,14 +98,31 @@ class UserForm(Static):
                         email=email,
                         full_name=full_name,
                         hashed_password=password,
-                        department=department
-                        if isinstance(department, DepartmentEnum)
-                        else DepartmentEnum.guest,
+                        department=department,
                         is_active=is_active,
                     )
-                    # self.crud_user.create(db, obj_in=user)
-                    print(user)
+                    self.crud_user.create(db, obj_in=user)
                 except Exception as e:
                     self.log.warning(e)
             self.query(Container).remove()
             self.mount(UserList())
+
+
+class UserFormUpdate(Static):
+    def __init__(self, **kwargs) -> None:
+        self.crud_user = CRUDUser(User)
+        super().__init__(**kwargs)
+
+    def compose(self) -> ComposeResult:
+        yield Container(
+            Input(placeholder="Nom complet", id="full_name"),
+            Input(placeholder="Email", id="email"),
+            Select.from_values(LINES, prompt="Departement", id="department"),
+            Checkbox(label="Actif", value=True, id="is_active"),
+            id="form",
+        )
+        yield Container(
+            Button("Annuler", variant="warning", id="cancel_user"),
+            Button("Modifier", variant="success", id="update_user"),
+            classes="buttons",
+        )
